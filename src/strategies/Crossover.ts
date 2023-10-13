46480a586359e6b2872574db483c1866ce87c82e
chakri68 <chakridevireddy69@gmail.com>
2023-10-13 13:48:49 +0530

    Fixed Crossover correctness

--- src/strategies/Crossover.ts
+++ src/strategies/Crossover.ts
@@ -18,26 +18,26 @@ export function CrossoverGenerator(world: World) {
       motherGenome,
       fatherGenome
     ) => {
-      const crossoverPosition = getRandomNumber(0, motherGenome.length);
-      const offspring1: Genome = [];
-      const offspring2: Genome = [];
+      const crossoverPosition = getRandomNumber(1, motherGenome.length - 1);
+      const offspring1Genome: Genome = [];
+      const offspring2Genome: Genome = [];
 
       // Generate genome for offspring1 -> Head from mother and tail from father
-      offspring1.push(...motherGenome.slice(0, crossoverPosition));
-      offspring1.push(
+      offspring1Genome.push(...motherGenome.slice(0, crossoverPosition));
+      offspring1Genome.push(
         ...fatherGenome.filter(
-          (chromosome) => offspring1.indexOf(chromosome) === -1
+          (chromosome) => offspring1Genome.indexOf(chromosome) === -1
         )
       );
 
       // Generate genome for offspring2 -> Head from father and tail from mother
-      offspring2.push(...fatherGenome.slice(0, crossoverPosition));
-      offspring2.push(
+      offspring2Genome.push(...fatherGenome.slice(0, crossoverPosition));
+      offspring2Genome.push(
         ...motherGenome.filter(
-          (chromosome) => offspring1.indexOf(chromosome) === -1
+          (chromosome) => offspring2Genome.indexOf(chromosome) === -1
         )
       );
-      return [offspring1, offspring2];
+      return [offspring1Genome, offspring2Genome];
     };
 
     // Implement more crossover methods
